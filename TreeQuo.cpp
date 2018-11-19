30467d62c165756a803b42e6207ec74503111df8
Raaghav0 <es15btech11021@iith.ac.in>
2018-11-20 01:50:13 +0530

    Add files via upload

--- TreeQuo.cpp
+++ TreeQuo.cpp
@@ -10,6 +10,7 @@
 #include<string>
 #include<queue>
 #include<mutex>
+#include<sys/time.h>
 using namespace std;
 int n; // number of nodes
 atomic<bool> inCS;
@@ -23,21 +24,24 @@ atomic<int> m_count;
 long int initTime;
 struct reqMsg
 {
-  int p_id;
-  int timestamp;
+  long long int p_id;
+  long long int timestamp;
   bool operator<(const reqMsg& rhs) const
   {
-    return timestamp > rhs.timestamp;
+    if(timestamp != rhs.timestamp)
+      return timestamp > rhs.timestamp;
+    else
+      return p_id > rhs.p_id;
   }
 };
 priority_queue<reqMsg> requestQueue;
 priority_queue<reqMsg> delayedQueue;
 
-int timestamp()
+long long int timestamp()
 {
-  time_t t = time(0);
-  long int t1 = static_cast<long int> (t);
-  return (t1-initTime)%INT_MAX;
+  struct timeval t;
+  gettimeofday(&t, NULL);
+  return t.tv_sec * 1000000 +  t.tv_usec;
 }
 char* getTime() // Retuns current time as hours : minute : seconds string
 {
@@ -63,7 +67,7 @@ vector<int> GenerateQuorums(int nodeId, vector<int> quorum)
   else
   {
     vector<int> leftPath = GenerateQuorums( nodeId*2, quorum);
-    vector<int> rightPath = GenerateQuorums( nodeId*2, quorum);
+    vector<int> rightPath = GenerateQuorums( nodeId*2+1, quorum);
     if(checkFail && leftPath.size()!=0 && rightPath.size() !=0)
     {
       leftPath.insert(leftPath.end(),rightPath.begin(), rightPath.end());
@@ -84,9 +88,21 @@ vector<int> GenerateQuorums(int nodeId, vector<int> quorum)
     }
   }
 }
+vector<int> GenerateQuorum(int nodeId,vector<int> quorum)
+{
 
+  int a = 1;
+  while(a<n)
+  {
+    quorum.push_back(a);
+    a = a*2;
+  }
+  return quorum;
+
+}
 void enterCS(int id) // Repeated CS request after some sleep time for every process
 {
+  // cout << "\n" << "enterCS started for" << id  << endl;
   ofstream log_file("TreeQuorum-log.txt", ios::app);
   for(int i=0;i<k;i++)
   {
@@ -95,77 +111,143 @@ void enterCS(int id) // Repeated CS request after some sleep time for every proc
     string s = "p" + to_string(id) + " requests CS  " + " at " + string(getTime()) + "\n";
     log_file << s <<  flush;
     vector<int> a;
-    quorums = GenerateQuorums(1,a);
+    quorums = GenerateQuorum(1,a);
+    // quorums = a;
     int l = quorums.size();
     unordered_map<int,bool> quo;
     quorumGrants = quo;
+    // cout << "\n" << "Reached here " << id << endl;
+    long long int timest = timestamp();
     for(int i=0;i<l;i++)
     {
       quorumGrants[quorums[i]]= false;
-      int b[2];
-      b[0] = 1;
-      b[1] = timestamp();
-      MPI_Send(&b,2,MPI_INT, quorums[i], 0, MPI_COMM_WORLD);// send a request message
-      m_count++;
+      if( quorums[i] != id)
+      {
+        long long int b[2];
+        b[0] = 1;
+        b[1] = timest;
+        MPI_Send(&b,2,MPI_LONG_LONG_INT, quorums[i], 0, MPI_COMM_WORLD);// send a request message
+        //cout << "\n" << b[1] << "is the timestamp to " << quorums[i] << "from " << id << endl << flush;
+        m_count++;
+      }
+      else
+      {
+        queueLock.lock();
+        if(requestQueue.empty())
+        {
+          quorumGrants[quorums[i]] = true;
+          cout << "\n" << "Granted " << id << " CS by " << id << endl << flush;
+          requestQueue.push(reqMsg{id,timest});
+        }
+        else
+        {
+          if(timest <  requestQueue.top().timestamp || (id < requestQueue.top().p_id && timest ==  requestQueue.top().timestamp ))
+          {
+            long long int b[2];
+            b[0] = 2;
+            b[1] = timestamp();
+            if(delayedQueue.empty())
+              MPI_Send(&b,2,MPI_LONG_LONG_INT, requestQueue.top().p_id, 0, MPI_COMM_WORLD);// send an inquire message
+            m_count++;
+            delayedQueue.push(reqMsg{id,timest});
+          }
+          else
+          {
+            requestQueue.push(reqMsg{id,timest});
+          }
+        }
+        queueLock.unlock();
+      }
     }
+    // cout << "\n" << "Sent request to quorums by " << id << " at " << timest << endl;
     while(!inCS)
     {
       int a=0;
-      for(auto i : quorumGrants)
+      for(int i=0;i<l;i++)
       {
-        if(i.second)
+        if(quorumGrants[quorums[i]])
           a++;
-        else
-          break;
       }
       if( a == l)
         inCS = true;
+      // else if( a == l-1)
+      //   cout << "Waiting for 1 more by " << id << endl << flush;
     }
     // Entered CS
     s = "p" + to_string(id) + " entered CS " + " at " + string(getTime()) + "\n";
     log_file << s <<  flush;
     usleep(20); // sleep for CS
     // Begin to Exit CS
+    inCS = false;
+    timest = timestamp();
     for(int i=0;i<l;i++)
     {
-      int b[2];
-      b[0] = 4;
-      b[1] = timestamp();
-      MPI_Send(&b,2,MPI_INT, quorums[i], 0, MPI_COMM_WORLD);// send a release message
-      m_count++;
+      if(quorums[i] != id)
+      {
+        long long int b[2];
+        b[0] = 4;
+        b[1] = timest;
+        MPI_Send(&b,2,MPI_LONG_LONG_INT, quorums[i], 0, MPI_COMM_WORLD);// send a release message
+        // cout << "\n" << b[1] << "is the release timestamp to " << quorums[i] << "from " << id << endl << flush;
+        m_count++;
+      }
+      else
+      {
+        requestQueue.pop();
+        while(!delayedQueue.empty())
+        {
+          requestQueue.push(delayedQueue.top());
+          delayedQueue.pop();
+        }
+        if(!requestQueue.empty())
+        {
+          long long int b[2];
+          b[0] = 0;
+          b[1] = timestamp();
+          MPI_Send(&b,2, MPI_LONG_LONG_INT, requestQueue.top().p_id, 0, MPI_COMM_WORLD);// send grant message
+          cout << "\n" << "Granted " << requestQueue.top().p_id << " CS by " << id << endl << flush;
+          m_count++;
+        }
+      }
     }
-    // Exit CS
+    // Exit CS msg
     s = "p" + to_string(id) + " exits CS " +  " at " + string(getTime()) + "\n";
     log_file << s <<  flush;
-    inCS = false;
   }
   //Send Terminate message from this process perspective to each process
   for(int i=1;i<=n;i++)
   {
     if(i!= id)
     {
-      int b[2];
+      long long int b[2];
       b[0] = 5;
       b[1] = timestamp();
-      MPI_Send(&b,2,MPI_INT, i, 0, MPI_COMM_WORLD);// send a release message
+      MPI_Send(&b,2,MPI_LONG_LONG_INT, i, 0, MPI_COMM_WORLD);// send a release message
       m_count++;
     }
     else
       terminated[i] = true;
   }
   log_file.close();
+  cout << "p" << id << " has completed its CS needs" << endl << flush;
+  while(!term);
+
+  int a = m_count+1; // send message complexity to initializing node
+  MPI_Send(&a,1, MPI_INT, 0, 0 , MPI_COMM_WORLD);
+  cout << "p" << id << " has reached termination" << endl << flush;
 }
 void recvMsg(int id, int p_id) // Receiving messages by process 'id'  from process 'p_id'
 {
-  while(!term)
+  // cout << "\n" << "recvmsg started " << id << "   " << p_id << endl<< flush;
+  while(true)
   {
-    int a[2];
-    MPI_Recv(&a,2, MPI_INT, p_id,0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    long long int a[2];
+    MPI_Recv(&a,2, MPI_LONG_LONG_INT, p_id,0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    cout << "\n" << "received " << a[0] << "     " << a[1] << " in " << id << "   " << p_id << endl << flush;
     // if grant
     if(a[0] == 0)
     {
-      if(quorumGrants.count(p_id) != 0)
-        quorumGrants[p_id] = true;
+      quorumGrants[p_id] = true;
     }
     // if request
     else if(a[0] == 1)
@@ -173,29 +255,61 @@ void recvMsg(int id, int p_id) // Receiving messages by process 'id'  from proce
       if(requestQueue.empty())
       {
         queueLock.lock();
-        requestQueue.push(reqMsg{p_id,a[0]});
-        int b[2];
+        requestQueue.push(reqMsg{p_id,a[1]});
+        long long int b[2];
         b[0] = 0;
         b[1] = timestamp();
-        MPI_Send(&b,2, MPI_INT, requestQueue.top().p_id, 0, MPI_COMM_WORLD);// send grant message
+        MPI_Send(&b,2, MPI_LONG_LONG_INT, requestQueue.top().p_id, 0, MPI_COMM_WORLD);// send grant message
+        cout << "\n" << "Granted " << requestQueue.top().p_id << " CS by " << id << endl << flush;
         m_count++;
         queueLock.unlock();
       }
-      else if(a[1] <  requestQueue.top().timestamp)
+      else if(a[1] <  requestQueue.top().timestamp || (p_id < requestQueue.top().p_id && a[1] ==  requestQueue.top().timestamp ))
       {
-        int b[2];
+        long long int b[2];
         b[0] = 2;
         b[1] = timestamp();
-        MPI_Send(&b,2,MPI_INT, requestQueue.top().p_id, 0, MPI_COMM_WORLD);// send an inquire message
-        m_count++;
-        queueLock.lock();
-        delayedQueue.push(reqMsg{p_id,a[0]});
-        queueLock.unlock();
+        if(requestQueue.top().p_id == id )
+        {
+          if(!inCS)
+          {
+            quorumGrants[id] = false;
+            queueLock.lock();
+            while(!delayedQueue.empty())
+            {
+              requestQueue.push(delayedQueue.top());
+              delayedQueue.pop();
+            }
+            requestQueue.push(reqMsg{p_id,a[1]});
+            long long int b[2];
+            b[0] = 0;
+            b[1] = timestamp();
+            MPI_Send(&b,2, MPI_LONG_LONG_INT, requestQueue.top().p_id, 0, MPI_COMM_WORLD);// send grant message
+            cout << "\n" << "Granted " << requestQueue.top().p_id << " CS by " << id << endl << flush;
+            m_count++;
+            queueLock.unlock();
+          }
+          else
+          {
+            queueLock.lock();
+            delayedQueue.push(reqMsg{p_id,a[1]});
+            queueLock.unlock();
+          }
+        }
+        else
+        {
+          queueLock.lock();
+          if(delayedQueue.empty())
+            MPI_Send(&b,2,MPI_LONG_LONG_INT, requestQueue.top().p_id, 0, MPI_COMM_WORLD);// send an inquire message
+          m_count++;
+          delayedQueue.push(reqMsg{p_id,a[1]});
+          queueLock.unlock();
+        }
       }
       else
       {
         queueLock.lock();
-        requestQueue.push(reqMsg{p_id,a[0]});
+        requestQueue.push(reqMsg{p_id,a[1]});
         queueLock.unlock();
       }
     }
@@ -206,10 +320,10 @@ void recvMsg(int id, int p_id) // Receiving messages by process 'id'  from proce
       {
         if(quorumGrants.count(p_id) != 0)
           quorumGrants[p_id] = false;
-        int b[2];
+        long long int b[2];
         b[0] = 3;
         b[1] = timestamp();
-        MPI_Send(&b,2,MPI_INT, p_id, 0, MPI_COMM_WORLD);// send a yield message
+        MPI_Send(&b,2,MPI_LONG_LONG_INT, p_id, 0, MPI_COMM_WORLD);// send a yield message
       }
     }
     // if yield
@@ -222,11 +336,20 @@ void recvMsg(int id, int p_id) // Receiving messages by process 'id'  from proce
         delayedQueue.pop();
       }
       queueLock.unlock();
-      int b[2];
-      b[0] = 1;
-      b[1] = timestamp();
-      MPI_Send(&b,2,MPI_INT, requestQueue.top().p_id, 0, MPI_COMM_WORLD);// send an inquire message
-      m_count++;
+      if(requestQueue.top().p_id != id)
+      {
+        long long int b[2];
+        b[0] = 0;
+        b[1] = timestamp();
+        MPI_Send(&b,2,MPI_LONG_LONG_INT, requestQueue.top().p_id, 0, MPI_COMM_WORLD);// send a grant  message
+        cout << "\n" << "Granted " << requestQueue.top().p_id << " CS by " << id << endl << flush;
+        m_count++;
+      }
+      else
+      {
+        quorumGrants[id] = true;
+        cout << "\n" << "Granted " << requestQueue.top().p_id << " CS by " << id << endl << flush;
+      }
     }
 
     // if release
@@ -235,13 +358,30 @@ void recvMsg(int id, int p_id) // Receiving messages by process 'id'  from proce
       if(requestQueue.top().p_id == p_id)
       {
         requestQueue.pop();
+        while(!delayedQueue.empty())
+        {
+          requestQueue.push(delayedQueue.top());
+          delayedQueue.pop();
+        }
         if(!requestQueue.empty())
         {
-          int b[2];
-          b[0] = 0;
-          b[1] = timestamp();
-          MPI_Send(&b,2, MPI_INT, requestQueue.top().p_id, 0, MPI_COMM_WORLD);// send grant message
-          m_count++;
+          if(requestQueue.top().p_id != id)
+          {
+            long long int b[2];
+            b[0] = 0;
+            b[1] = timestamp();
+            MPI_Send(&b,2, MPI_LONG_LONG_INT, requestQueue.top().p_id, 0, MPI_COMM_WORLD);// send grant message
+            cout << "\n" << "Granted " << requestQueue.top().p_id << " CS by " << id << endl << flush;
+            m_count++;
+          }
+          else
+          {
+            if(quorumGrants.count(id) != 0)
+            {
+              quorumGrants[id] = true;
+              cout << "\n" << "Granted " << requestQueue.top().p_id << " CS by " << id << endl << flush;
+            }
+          }
         }
       }
     }
@@ -251,59 +391,64 @@ void recvMsg(int id, int p_id) // Receiving messages by process 'id'  from proce
       int i=1;
       while(i<=n)
       {
-        if(!terminated[p_id])
+        if(!terminated[i])
           break;
       }
       if(i==n && terminated[n])
         term = true;
     }
   }
-  int a = m_count+1; // send message complexity to initializing node
-  MPI_Send(&a,1, MPI_INT, 0, 0 , MPI_COMM_WORLD);
+
 }
 void coordProc(int p_id)
 {
   int m;
   MPI_Recv(&m,1, MPI_INT, p_id,0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
   m_count = m_count + m;
+  cout << "Node " << p_id << "has return its message complexity" << endl << flush;
 }
 int main()
 {
   int provided;
   MPI_Init_thread(NULL, NULL, MPI_THREAD_MULTIPLE, &provided);
-  int p_id,n, p_size;
+  int p_id, p_size;
   MPI_Comm_rank(MPI_COMM_WORLD, &p_id);
   MPI_Comm_size(MPI_COMM_WORLD, &p_size);
   ofstream log_file("TreeQuorum-log.txt",ios::out);
   log_file.close();
-  ifstream input_file("inp_params.txt");
+  ifstream input_file("inp-params.txt");
   input_file >> n >> k;
   input_file.close();
   time_t t = time(0);
   initTime = static_cast<long int> (t);
+  // cout << "\n" << "inputs : " << n << k << endl;
+  // cout << "\n" << p_id <<  "   " << p_size << endl;
   if(p_id == 0)
   {
+    // cout << "\n" << "Case for " << p_id << endl;
     thread a[n];
-    cout << "Starting threads for " << p_id << endl;
+    // cout << "\n" << "Starting threads for " << p_id << endl;
     for(int i=0;i<n;i++)
       a[i] = thread(coordProc, i+1);
+    // cout << "\n" << "Closing threads for " << p_id << endl;
     for(int i=0;i<n;i++)
       a[i].join();
+    // cout << "\n" << "Closed threads for " << p_id << endl;
     ofstream outfile("Message Complexity.txt", ios::out);
     outfile << "Message Complexity : " << m_count << endl;
     outfile.close();
   }
-  if(p_id > 0 && p_id <=n)
+  if(p_id > 0 && p_id <= n)
   {
-    vector<bool> t(n);
-    for(int i=0;i<n;i++)
-      t[i] = false;
-    terminated = t;
+    // cout << "\n" << "Case for " << p_id << endl;
+    vector<bool> b(n+1);
+    for(int i=0;i<n+1;i++)
+      b[i] = false;
+    terminated = b;
     term = false;
     inCS = false;
     thread a[n]; // n-1 receivers, 1 CS thread
-    cout << "Starting threads for " << p_id << endl;
-    a[0] = thread(enterCS,p_id);
+    // cout << "\n" << "Starting threads for " << p_id << endl;
     int j = 1;
     for(int i=1;i<=n;i++)
     {
@@ -313,10 +458,16 @@ int main()
         j++;
       }
     }
+    usleep(10000);
+    a[0] = thread(enterCS,p_id);
+    // cout << "\n" << "Closing threads for " << p_id << endl;
+    a[0].join();
     for(int i=0;i<n;i++)
-      a[i].join();
+      a[i].detach();
+    // cout << "\n" << "Closedthreads for " << p_id << endl;
   }
+  cout << "\n" << "Reached end " << p_id << endl;
   return 0;
   MPI_Finalize();
-
+  return 0;
 }
